11bc891a053e2fd0c8dfd6cf5b7b0d1466951553
Kelvim Imperial <74459989+KelvimImperial@users.noreply.github.com>
2023-05-23 08:30:25 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,35 @@
+﻿import "package:flutter/material.dart";
+import 'chat/screens/home_screen.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+
+import 'dart:math';
+import "./components/transaction_form.dart";
+import "./components/transaction_list.dart";
+import "./models/transaction.dart";
+import 'Design_com_flutter/teste1.dart';
+import 'login/animacao_login_menu.dart';
+import 'login/login_criarConta.dart';
+
+
+main() {
+  runApp(MaterialApp(
+    home:MyApp ()
+  ));
+}
+
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Flutter Chat UI",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            //brightness: Brightness.dark,
+            primaryColor: Colors.red,
+            accentColor: Color(0xFFFEF9EB)),
+        home: HomeScreen());
+  }
+}
